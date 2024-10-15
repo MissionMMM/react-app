bb8b5a0c8fc8c819d6d3d48f92ae60925983b469
弥森 <2475905645@qq.com>
2024-10-15 17:15:28 +0800

    logo调整

--- src/pages/components/Logo.js
+++ src/pages/components/Logo.js
@@ -3,10 +3,14 @@ import './Logo.css'
 function Logo(){
     return(
         <div className="logoBox">
-            <div>W</div>
-            <div>e</div>
-            <div className='importFont'>D</div>
-            <div>o</div>
+            <div>H</div>
+            <div>E</div>
+            <div>D</div>
+            <div>G</div>
+            <div>E</div>
+            <div className='importFont'>H</div>
+            <div className='importFont'>O</div>
+            <div className='importFont'>G</div>
         </div>
     )
 }
