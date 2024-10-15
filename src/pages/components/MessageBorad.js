c0d92763705ba96d45a7843558b5b7eb83d26db0
弥森 <2475905645@qq.com>
2024-10-15 16:32:18 +0800

    留言板临时记录调整

--- src/pages/components/MessageBorad.js
+++ src/pages/components/MessageBorad.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { get } from "../../utils/request"
 import "./MessageBorad.css"
 
+import { traditionalized } from "../../utils/simpleTraditionalizedExchange"
+
 function MessageBorad() {
     const [messageList, setMassageList] = useState([])
 
@@ -32,11 +34,11 @@ function MessageBorad() {
             <div className="message-borad-content">
                 <div className="message-borad-item">
                     <div className="message-item-title-box">
-                        <div className="message-item-text-1">用户昵称</div>
-                        <div className="message-item-text-2">2024-10-15</div>
+                        <div className="message-item-text-1">{traditionalized('弥森')}</div>
+                        <div className="message-item-text-2">2024-10-15 16:31</div>
                     </div>
-                    <div className="message-item-address">地区：深圳</div>
-                    <div className="message-item-content">我是消息内容的洒家打开手机按常理撒娇零地价萨兰迪加萨兰迪加萨里的加萨里的i加萨莉打算</div>
+                    <div className="message-item-address">{traditionalized('地区：深圳')}</div>
+                    <div className="message-item-content">{traditionalized('welcome to my blog，虽然现在还什么都没有，但一切都是暂时的，好的也好坏的也罢')}</div>
                     <div className="message-borad-corner" />
                 </div>
             </div>
